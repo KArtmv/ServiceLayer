e50f2ddfd808b805f16ba5169d38308674a15cab
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-11 15:38:45 +0200

    Initial commit

---
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/ServiceLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.javaspring.ServiceLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
