3e3141829990c65571d9e6e0a7576b5845c331bb
”Kyrylo <kirillartemov1993@gmail.com>
2023-07-17 11:07:43 +0200

    interface of student dao

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/dao/StudentDAO.java
@@ -0,0 +1,20 @@
+package ua.foxminded.javaspring.ServiceLayer.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import ua.foxminded.javaspring.ServiceLayer.model.Student;
+import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
+
+public interface StudentDAO {
+
+	Optional<Student> getStudentByID(Student student);
+
+	boolean deleleteStudent(Student student);
+
+	List<StudentAtCourse> studentCourses(Student student);
+
+	boolean addStudent(Student student);
+
+	boolean isValidStudentID(Student student);
+}
