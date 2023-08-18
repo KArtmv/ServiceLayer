4b8eae9ed26a1d72f4d0caec8f8cca98feafcb7e
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-18 19:43:47 +0200

    Add CounterStudentsAtGroup
    -Created CounterStudentsAtGroup class to store data on the number of
    students and on which course

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/model/CounterStudentsAtGroup.java
@@ -0,0 +1,20 @@
+package ua.foxminded.javaspring.ServiceLayer.model;
+
+public class CounterStudentsAtGroup {
+
+	private Integer studentsCount;
+	private String groupName;
+
+	public CounterStudentsAtGroup(Integer studentsCount, String groupName) {
+		this.studentsCount = studentsCount;
+		this.groupName = groupName;
+	}
+
+	public Integer getStudentsCount() {
+		return studentsCount;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+}
