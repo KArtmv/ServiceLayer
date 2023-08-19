a21af05d9fda5e7af63b0ceaef2e793b37b34a51
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-19 10:46:27 +0200

    Add row mapper classes
    -created classes which put data from database row to entities

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/rowmapper/CountStudentAtGroupMapper.java
@@ -0,0 +1,18 @@
+package ua.foxminded.javaspring.ServiceLayer.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ua.foxminded.javaspring.ServiceLayer.model.CounterStudentsAtGroup;
+
+public class CountStudentAtGroupMapper implements RowMapper<CounterStudentsAtGroup> {
+
+	@Override
+	public CounterStudentsAtGroup mapRow(ResultSet rs, int rowNum) throws SQLException {
+		return new CounterStudentsAtGroup(
+				rs.getInt("coutn"),
+				rs.getString("course_name"));
+	}
+}
