898b2168b1c9ebe6ec82ff21b8cd7f6431b9c0b6
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-21 16:36:52 +0200

    Add Generator database data
    -Created class which check is the table and insert generated data into
    database

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/GeneratorDatabaseData.java
@@ -0,0 +1,60 @@
+package ua.foxminded.javaspring.ServiceLayer.data;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Component;
+
+import ua.foxminded.javaspring.ServiceLayer.data.tables.TablesConduct;
+import ua.foxminded.javaspring.ServiceLayer.model.Course;
+import ua.foxminded.javaspring.ServiceLayer.model.Group;
+import ua.foxminded.javaspring.ServiceLayer.model.Student;
+import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
+
+@Component
+public class GeneratorDatabaseData {
+
+	private InsertDataIntoDatabase dataIntoDatabase;
+	private TablesConduct checkIsExist;
+	private DataConduct dataConduct;
+
+	public GeneratorDatabaseData(InsertDataIntoDatabase dataIntoDatabase, TablesConduct conduct,
+			DataConduct dataConduct) {
+		this.dataIntoDatabase = dataIntoDatabase;
+		this.checkIsExist = conduct;
+		this.dataConduct = dataConduct;
+	}
+
+	@PostConstruct
+	public void generateDataAndInsertIntoDatabase() {
+		group(dataConduct.createGroups());
+		student(dataConduct.createStudents());
+		course(dataConduct.createCourses());
+		studentToCourse(dataConduct.createRelationStudentCourse());
+	}
+
+	private void student(List<Student> students) {
+		if (!checkIsExist.studentTable()) {
+			dataIntoDatabase.insertStudent(students);
+		}
+	}
+
+	private void group(List<Group> groups) {
+		if (!checkIsExist.groupTable()) {
+			dataIntoDatabase.insertGroup(groups);
+		}
+	}
+
+	private void course(List<Course> courses) {
+		if (!checkIsExist.courseTable()) {
+			dataIntoDatabase.insertCourse(courses);
+		}
+	}
+
+	private void studentToCourse(List<StudentAtCourse> studentAtCourses) {
+		if (!checkIsExist.studentToCourseTable()) {
+			dataIntoDatabase.insertStudentToCourse(studentAtCourses);
+		}
+	}
+}
