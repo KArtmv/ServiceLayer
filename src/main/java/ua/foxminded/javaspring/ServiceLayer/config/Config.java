624619b9a2f6e96a7ca60416888e842e37358ffe
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-21 15:55:04 +0200

    Add configuration
    -Created bean configuration of random and the maximum number of courses
    for one student

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/config/Config.java
@@ -0,0 +1,21 @@
+package ua.foxminded.javaspring.ServiceLayer.config;
+
+import java.util.Random;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+	private static final Integer MAX_COUNT_COURSES_OF_STUDENT = 3;
+
+	@Bean
+	public Random random() {
+		return new Random();
+	}
+
+	@Bean
+	public Integer maxCountCoursesOfStudent() {
+		return MAX_COUNT_COURSES_OF_STUDENT;
+	}
+}
