1d6fb6207bb9b71b02be1c72898ee48876cd81d1
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-21 16:37:26 +0200

    Fix of grammar

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/RandomNumber.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/RandomNumber.java
@@ -13,7 +13,7 @@ public class RandomNumber {
 		this.random = random;
 	}
 
-	public Integer generateBeetwenOneAnd(int toNumber) {
+	public Integer generateBetweenOneAnd(int toNumber) {
 		int randomNumber = 0;
 		boolean isZero = false;
 		while (!isZero) {
