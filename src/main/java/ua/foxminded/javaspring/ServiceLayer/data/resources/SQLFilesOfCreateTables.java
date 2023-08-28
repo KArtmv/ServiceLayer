2848578c544f99fee9d59313420d4507f06956fa
”Kyrylo <kirillartemov1993@gmail.com>
2023-08-28 12:41:41 +0200

    Change SQLFilesOfCreateTables
    -Correct file path
    -Move to underfolder */tables/

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/SQLFilesOfCreateTables.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/SQLFilesOfCreateTables.java
@@ -2,10 +2,10 @@ package ua.foxminded.javaspring.ServiceLayer.data.resources;
 
 public class SQLFilesOfCreateTables {
 
-	private static final String SQL_SCRIPT_FILE_STUDENT = "src/main/resources/studentTable.txt";
-	private static final String SQL_SCRIPT_FILE_GROUP = "src/main/resources/groupTable.txt";
-	private static final String SQL_SCRIPT_FILE_COURSE = "src/main/resources/courseTable.txt";
-	private static final String SQL_SCRIPT_FILE_STUDENT_TO_COURSE = "src/main/resources/studentToCourseTable.txt";
+	private static final String SQL_SCRIPT_FILE_STUDENT = "src/main/resources/tables/studentTable.txt";
+	private static final String SQL_SCRIPT_FILE_GROUP = "src/main/resources/tables/groupTable.txt";
+	private static final String SQL_SCRIPT_FILE_COURSE = "src/main/resources/tables/courseTable.txt";
+	private static final String SQL_SCRIPT_FILE_STUDENT_TO_COURSE = "src/main/resources/tables/studentToCourseTable.txt";
 
 	public String getSqlScriptFileStudent() {
 		return SQL_SCRIPT_FILE_STUDENT;
