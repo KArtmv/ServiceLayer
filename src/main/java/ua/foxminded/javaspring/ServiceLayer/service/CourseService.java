338831860a6fb4b40b914acccad44327efd568e2
”Kyrylo <kirillartemov1993@gmail.com>
2023-09-04 18:00:01 +0200

    Create CourseService interface

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/service/CourseService.java
@@ -0,0 +1,7 @@
+package ua.foxminded.javaspring.ServiceLayer.service;
+
+import ua.foxminded.javaspring.ServiceLayer.model.Course;
+
+public interface CourseService {
+	boolean isValidCourseID(Course course);
+}
