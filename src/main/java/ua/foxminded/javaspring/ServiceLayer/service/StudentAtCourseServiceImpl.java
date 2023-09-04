f1c12c945a7b0f74ff04a1efe07083e23d3cf7f6
”Kyrylo <kirillartemov1993@gmail.com>
2023-09-04 18:02:43 +0200

    Implement of StudentAtCourseService interface

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/service/StudentAtCourseServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/service/StudentAtCourseServiceImpl.java
@@ -2,36 +2,36 @@ package ua.foxminded.javaspring.ServiceLayer.service;
 
 import java.util.List;
 
-import ua.foxminded.javaspring.ServiceLayer.dao.CourseDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.ServiceLayer.model.Course;
 import ua.foxminded.javaspring.ServiceLayer.model.Student;
 import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
 
-public class CourseServiceImpl implements CourseService {
+public class StudentAtCourseServiceImpl implements StudentAtCourseService {
 
-	private CourseDAO courseDAO;
+	private StudentAtCourseDAO studentAtCourseDAO;
 
-	public CourseServiceImpl(CourseDAO courseDAO) {
-		this.courseDAO = courseDAO;
+	public StudentAtCourseServiceImpl(StudentAtCourseDAO studentAtCourseDAO) {
+		this.studentAtCourseDAO = studentAtCourseDAO;
 	}
 
 	@Override
 	public List<StudentAtCourse> courseStudents(Course course) {
-		return courseDAO.courseStudents(course);
+		return studentAtCourseDAO.courseStudents(course);
 	}
 
 	@Override
 	public boolean addStodentToCourse(Student student, Course course) {
-		return courseDAO.addStodentToCourse(student, course);
+		return studentAtCourseDAO.addStodentToCourse(student, course);
 	}
 
 	@Override
 	public boolean removeStudentFromCourse(StudentAtCourse studentAtCourse) {
-		return courseDAO.removeStudentFromCourse(studentAtCourse);
+		return studentAtCourseDAO.removeStudentFromCourse(studentAtCourse);
 	}
 
 	@Override
 	public boolean removeStudentFromAllTheirCourses(Student student) {
-		return courseDAO.removeStudentFromAllTheirCourses(student);
+		return studentAtCourseDAO.removeStudentFromAllTheirCourses(student);
 	}
 }
