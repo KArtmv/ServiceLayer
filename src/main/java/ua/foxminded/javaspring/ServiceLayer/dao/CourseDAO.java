c7d9e0a18c9bf44aff6d5a4cc8837882b5c07b6b
”Kyrylo <kirillartemov1993@gmail.com>
2023-09-04 17:53:25 +0200

    Change CourseDAO/Repo.class
    -Removed methods not related to course table
    -Remove methods relate to StudentToCourse table to
    StudentAtCourseDAO/Repo

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/dao/CourseDAO.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/dao/CourseDAO.java
@@ -1,21 +1,9 @@
 package ua.foxminded.javaspring.ServiceLayer.dao;
 
-import java.util.List;
-
 import ua.foxminded.javaspring.ServiceLayer.model.Course;
-import ua.foxminded.javaspring.ServiceLayer.model.Student;
-import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
 
 public interface CourseDAO {
 	boolean addCourse(Course course);
-
-	List<StudentAtCourse> courseStudents(Course course);
-
-	boolean addStodentToCourse(Student student, Course course);
-
-	boolean removeStudentFromCourse(StudentAtCourse studentAtCourse);
-
-	boolean removeStudentFromAllTheirCourses(Student student);
 
 	boolean isValidCourseID(Course course);
 
@@ -23,11 +11,5 @@ public interface CourseDAO {
 
 	void createCourseTable();
 
-	boolean isStudentToCourseTableExist();
-
-	void createStodentToCourseTable();
-
 	boolean isCourseTableEmpty();
-
-	boolean isStudentToCourseTableEmpty();
 }
--- src/main/java/ua/foxminded/javaspring/ServiceLayer/repository/CourseRepo.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/repository/CourseRepo.java
@@ -1,8 +1,6 @@
 package ua.foxminded.javaspring.ServiceLayer.repository;
 
 import java.sql.ResultSet;
-import java.util.List;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -11,9 +9,6 @@ import ua.foxminded.javaspring.ServiceLayer.data.ReadResoucesFile;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLFilesOfCreateTables;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLScriptTablesExist;
 import ua.foxminded.javaspring.ServiceLayer.model.Course;
-import ua.foxminded.javaspring.ServiceLayer.model.Student;
-import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
-import ua.foxminded.javaspring.ServiceLayer.rowmapper.StudentAtCourseMapper;
 
 @Repository
 public class CourseRepo implements CourseDAO {
@@ -24,15 +19,8 @@ public class CourseRepo implements CourseDAO {
 	private SQLFilesOfCreateTables sqlTableFile;
 
 	private static final String SQL_ADD_NEW_COURSE = "insert into courses (course_name, course_description) values (?, ?)";
-	private static final String SQL_GET_ALL_STUDENT_FROM_COURSE = "select c.course_name, c.course_description, s.first_name, s.last_name"
-			+ "from studenttocourse sc" + "join students s on s.student_id = sc.student_id"
-			+ "join courses c on c.course_id = sc.course_id" + "where c.course_id=1";
-	private static final String SQL_ADD_STUDENT_TO_COURSE = "insert into studenttocourse (student_id, course_id) values (?, ?)";
-	private static final String SQL_REMOVE_STUDENT_FROM_COURSE = "delete from studentatcourse where enrollment_id=?";
-	private static final String SQL_REMOVE_STUDENT_FROM_ALL_THEIR_COURSES = "delete from studentatcourse where student_id=?";
 	private static final String SQL_CHECK_IS_COURSE_EXIST = "select course_id from course where course_id=?";
 	private static final String SQL_CHECK_IS_COURSE_TABLE_EMPTY = "SELECT COUNT(*) FROM courses";
-	private static final String SQL_CHECK_IS_STUDENT_TO_COURSE_TABLE_EMPTY = "SELECT COUNT(*) FROM studenttocourse";
 
 	public CourseRepo(JdbcTemplate jdbcTemplate, ReadResoucesFile readFile, SQLScriptTablesExist scriptTablesExist,
 			SQLFilesOfCreateTables sqlTableFile) {
@@ -40,26 +28,6 @@ public class CourseRepo implements CourseDAO {
 		this.readFile = readFile;
 		this.scriptTablesExist = scriptTablesExist;
 		this.sqlTableFile = sqlTableFile;
-	}
-
-	@Override
-	public List<StudentAtCourse> courseStudents(Course course) {
-		return jdbcTemplate.query(SQL_GET_ALL_STUDENT_FROM_COURSE, new StudentAtCourseMapper(), course.getCourseID());
-	}
-
-	@Override
-	public boolean addStodentToCourse(Student student, Course course) {
-		return jdbcTemplate.update(SQL_ADD_STUDENT_TO_COURSE, student.getStudentID(), course.getCourseID()) > 0;
-	}
-
-	@Override
-	public boolean removeStudentFromCourse(StudentAtCourse studentAtCourse) {
-		return jdbcTemplate.update(SQL_REMOVE_STUDENT_FROM_COURSE, studentAtCourse.getEnrollmentID()) > 0;
-	}
-
-	@Override
-	public boolean removeStudentFromAllTheirCourses(Student student) {
-		return jdbcTemplate.update(SQL_REMOVE_STUDENT_FROM_ALL_THEIR_COURSES, student.getStudentID()) > 0;
 	}
 
 	@Override
@@ -78,28 +46,12 @@ public class CourseRepo implements CourseDAO {
 	}
 
 	@Override
-	public boolean isStudentToCourseTableExist() {
-		return jdbcTemplate.queryForObject(scriptTablesExist.getStudentToCourseTableExist(), Boolean.class);
-	}
-
-	@Override
 	public void createCourseTable() {
 		jdbcTemplate.execute(readFile.getScript(sqlTableFile.getCourseFilePath()));
 	}
 
 	@Override
-	public void createStodentToCourseTable() {
-		jdbcTemplate.execute(readFile.getScript(sqlTableFile.getStudentToCourseFilePath()));
-	}
-
-	@Override
 	public boolean isCourseTableEmpty() {
 		return jdbcTemplate.queryForObject(SQL_CHECK_IS_COURSE_TABLE_EMPTY, Integer.class) == 0;
-
-	}
-
-	@Override
-	public boolean isStudentToCourseTableEmpty() {
-		return jdbcTemplate.queryForObject(SQL_CHECK_IS_STUDENT_TO_COURSE_TABLE_EMPTY, Integer.class) == 0;
 	}
 }
