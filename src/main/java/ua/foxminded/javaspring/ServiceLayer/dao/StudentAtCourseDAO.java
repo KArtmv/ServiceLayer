ace91c08d0c704303a51b1e5fc2617a5a58c40be
”Kyrylo <kirillartemov1993@gmail.com>
2023-09-04 17:57:48 +0200

    Create an interface for interacting with the StudentToCourse table

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/dao/StudentAtCourseDAO.java
@@ -0,0 +1,23 @@
+package ua.foxminded.javaspring.ServiceLayer.dao;
+
+import java.util.List;
+
+import ua.foxminded.javaspring.ServiceLayer.model.Course;
+import ua.foxminded.javaspring.ServiceLayer.model.Student;
+import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
+
+public interface StudentAtCourseDAO {
+	List<StudentAtCourse> courseStudents(Course course);
+	
+	boolean addStodentToCourse(Student student, Course course);
+	
+	boolean removeStudentFromCourse(StudentAtCourse studentAtCourse);
+	
+	boolean removeStudentFromAllTheirCourses(Student student);
+	
+	boolean isStudentToCourseTableExist();
+	
+	void createStodentToCourseTable();
+	
+	boolean isStudentToCourseTableEmpty();
+}
