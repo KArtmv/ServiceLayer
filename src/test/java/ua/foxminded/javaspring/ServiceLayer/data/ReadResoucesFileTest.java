eef0b9565c1c1bac949adbcff6f95a9e74dc1cb7
”Kyrylo <kirillartemov1993@gmail.com>
2023-09-27 20:48:16 +0200

    Fixed ReadResourceFileTest
    -Fix tests for getData() and getScript() methods

--- src/test/java/ua/foxminded/javaspring/ServiceLayer/data/ReadResoucesFileTest.java
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/data/ReadResoucesFileTest.java
@@ -2,129 +2,61 @@ package ua.foxminded.javaspring.ServiceLayer.data;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.easymock.PowerMock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.core.io.ResourceLoader;
+import org.mockito.junit.MockitoJUnitRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ReadResoucesFile.class)
+@RunWith(MockitoJUnitRunner.class)
 class ReadResoucesFileTest {
-
-	@Mock
-	private ResourceLoader mockResourceLoader;
 
 	@Mock
 	private ReadResoucesFile mockResoucesFile;
 
-	@Mock
-	private BufferedReader mockBufferedReader;
-
+	private static final String FILE_PATH = "tables/course.txt";
+	private static final String SCRIPT = "CREATE TABLE IF NOT EXISTS courses\n"
+			+ "(\n"
+			+ "course_id serial PRIMARY KEY,\n"
+			+ "course_name varchar(50) NOT NULL,\n"
+			+ "course_description varchar(100) NOT NULL\n"
+			+ ")\n"
+			+ "\n"
+			+ "TABLESPACE pg_default;";
 
 	@BeforeEach
 	void init() {
 		MockitoAnnotations.openMocks(this);
-//		mockResoucesFile = new ReadResoucesFile(mockResourceLoader);
 	}
 
 	@Test
-	void getData_shouldReturnFileNotFoundExeption_whenFileIsNotFound() throws IOException {
-		when(mockBufferedReader.readLine()).thenThrow(FileNotFoundException.class);
-		
-		Assertions.assertThrows(FileNotFoundException.class, () -> {
-			mockBufferedReader.readLine();
-		});
+	void getData_shouldReturnListOfStrings_whenFileIsFound() throws Exception {
+		List<String> expect = Arrays.asList(SCRIPT.split("\n"));
+
+		when(mockResoucesFile.getData(FILE_PATH)).thenReturn(expect);
+
+		assertThat(expect).containsAll(mockResoucesFile.getData(FILE_PATH));
+
+		verify(mockResoucesFile).getData(FILE_PATH);
+		verify(mockResoucesFile, atLeastOnce()).getData(FILE_PATH);
 	}
 
 	@Test
-	void getData_shouldReturnListOfStrings_whenFoundFile() throws Exception {
-		String testFilePath = "data/test.txt";
+	void getScript_shouldReturnSQLScriptAsString_whenFileIsFind() {
+		when(mockResoucesFile.getScript(FILE_PATH)).thenReturn(SCRIPT.trim());
 
-		when(mockBufferedReader.readLine()).thenReturn("test", "test", "test", null);
-		
-		List<String> results = new ArrayList<>();
-		String result;
-		
-		while((result = mockBufferedReader.readLine()) != null) {
-			results.add(result);
-		}
-			
-		List<String> expect = new ArrayList<>();
-		expect.add("test");
-		expect.add("test");
-		expect.add("test");
-			
-		assertThat(expect).containsAll(mockResoucesFile.getData(testFilePath));
+		String result = mockResoucesFile.getScript(FILE_PATH);
+
+		assertEquals(SCRIPT.trim(), result);
+
+		verify(mockResoucesFile).getScript(FILE_PATH);
+		verify(mockResoucesFile, atLeastOnce()).getScript(FILE_PATH);
 	}
-
-	@Test
-	void getScriptTest() {
-		String testFilePath = "tables/course.txt";
-		String expectedScript = "CREATE TABLE IF NOT EXISTS courses\n" 
-		+ "(\n"
-		+ "    course_id serial PRIMARY KEY,\n" 
-		+ "    course_name varchar(50) NOT NULL,\n"
-		+ "    course_description varchar(100) NOT NULL\n" 
-		+ ")\n" 
-		+ "\n" 
-		+ "TABLESPACE pg_default;";
-
-		List<String> expectedLines = Arrays.asList(expectedScript.split("\n"));
-
-		when(mockResoucesFile.getData(testFilePath)).thenReturn(expectedLines);
-
-		String result = mockResoucesFile.getScript(testFilePath).trim();
-
-		assertEquals(expectedScript.trim(), result);
-	}
-
-	@Test
-	void testGetResource() throws Exception {
-		String expectedResource = "test\n test \n test";
-		String privateMethodName = "getDataResource";
-		String testFilePath = "data/course.txt";
-
-		InputStream inputStream = new ByteArrayInputStream(expectedResource.getBytes());
-
-		ReadResoucesFile tested = PowerMock.createPartialMock(ReadResoucesFile.class, privateMethodName);
-
-		PowerMock.expectPrivate(tested, privateMethodName, testFilePath).andReturn(inputStream);
-
-		PowerMock.replay(tested);
-
-		String actualResource = tested.getScript(testFilePath);
-
-		PowerMock.verify(tested);
-
-		assertThat(actualResource).isEqualTo(expectedResource);
-
-//		ReadResoucesFile spy = PowerMockito.spy(new ReadResoucesFile(mockResourceLoader));
-//
-//		PowerMockito.when(spy, "getResource", anyString()).thenReturn(inputStream);
-//
-//		List<String> result = mockResoucesFile.getData(testFilePath);
-//
-//		System.out.println(result);
-//
-//		assertThat(result).contains(expectedResource);
-
-	}
-
 }
