22305bd033256c3cdfe5b28e48b41365acff3d1a
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-09 12:04:56 +0200

    Fix StudentAtCourseRepo.class -Mark it as @repository

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/repository/StudentAtCourseRepo.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/repository/StudentAtCourseRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.stereotype.Repository;
 import ua.foxminded.javaspring.ServiceLayer.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.ServiceLayer.data.ReadResourcesFile;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLFilesOfCreateTables;
@@ -13,6 +14,7 @@ import ua.foxminded.javaspring.ServiceLayer.model.Student;
 import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
 import ua.foxminded.javaspring.ServiceLayer.rowmapper.StudentAtCourseMapper;
 
+@Repository
 public class StudentAtCourseRepo implements StudentAtCourseDAO {
 
     private JdbcTemplate jdbcTemplate;
@@ -21,8 +23,10 @@ public class StudentAtCourseRepo implements StudentAtCourseDAO {
     private SQLFilesOfCreateTables sqlTableFile;
 
     private static final String SQL_GET_ALL_STUDENT_FROM_COURSE = "select c.course_name, c.course_description, s.first_name, s.last_name"
-            + "from studenttocourse sc" + "join students s on s.student_id = sc.student_id"
-            + "join courses c on c.course_id = sc.course_id" + "where c.course_id=1";
+            + "from studenttocourse sc"
+            + "join students s on s.student_id = sc.student_id"
+            + "join courses c on c.course_id = sc.course_id"
+            + "where c.course_id=1";
     private static final String SQL_ADD_STUDENT_TO_COURSE = "insert into studenttocourse (student_id, course_id) values (?, ?)";
     private static final String SQL_REMOVE_STUDENT_FROM_COURSE = "delete from studentatcourse where enrollment_id=?";
     private static final String SQL_REMOVE_STUDENT_FROM_ALL_THEIR_COURSES = "delete from studentatcourse where student_id=?";
