bcffa79dea886f6feddd92f39160f596b89f233e
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-09 11:46:09 +0200

    Remove @Bean maxCountCoursesOfStudent()

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/config/Config.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/config/Config.java
@@ -8,15 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
-    private static final Integer MAX_COUNT_COURSES_OF_STUDENT = 3;
-
     @Bean
     public Random random() {
         return new Random();
-    }
-
-    @Bean
-    public Integer maxCountCoursesOfStudent() {
-        return MAX_COUNT_COURSES_OF_STUDENT;
     }
 }
