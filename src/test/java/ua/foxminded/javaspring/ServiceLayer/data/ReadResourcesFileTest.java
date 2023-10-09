db0192530cb8eda9e60c862ff046b0a2c9f15846
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-09 12:00:58 +0200

    Correct ReadResourceFileTest.class -Remove @Mock ReadResourceFile -Add inject resourceLoader into int() -Fix name of tests -Add test of throws exception

--- src/test/java/ua/foxminded/javaspring/ServiceLayer/data/ReadResourcesFileTest.java
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/data/ReadResourcesFileTest.java
@@ -9,15 +9,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,8 +25,7 @@ class ReadResourcesFileTest {
     @Mock
     ResourceLoader resourceLoader;
 
-    @Mock
-    private ReadResourcesFile mockResourcesFile;
+    private ReadResourcesFile resourcesFile;
 
     private static final String FILE_PATH = "tables/course.txt";
     private static final String SCRIPT = "CREATE TABLE IF NOT EXISTS courses\n"
@@ -42,46 +40,46 @@ class ReadResourcesFileTest {
     @BeforeEach
     void init() {
         MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void getData_shouldReturnListOfStrings_whenFileIsFound() throws Exception {
-        List<String> expect = Arrays.asList(SCRIPT.split("\n"));
-
-        when(mockResourcesFile.getData(FILE_PATH)).thenReturn(expect);
-
-        assertThat(expect).containsAll(mockResourcesFile.getData(FILE_PATH));
-
-        verify(mockResourcesFile).getData(FILE_PATH);
-        verify(mockResourcesFile, atLeastOnce()).getData(FILE_PATH);
-    }
-
-    @Test
-    void getScript_shouldReturnSQLScriptAsString_whenFileIsFind() {
-        when(mockResourcesFile.getScript(FILE_PATH)).thenReturn(SCRIPT.trim());
-
-        String result = mockResourcesFile.getScript(FILE_PATH);
-
-        assertEquals(SCRIPT.trim(), result);
-
-        verify(mockResourcesFile).getScript(FILE_PATH);
-        verify(mockResourcesFile, atLeastOnce()).getScript(FILE_PATH);
+        resourcesFile = new ReadResourcesFile(resourceLoader);
     }
 
     @Test
     void getData_shouldReturnListOfString_whenIsValidDataProvided() throws IOException {
-        ReadResourcesFile resourcesFile = new ReadResourcesFile(resourceLoader);
-        Resource resource = mock(Resource.class);
-        InputStream inputStream = new ByteArrayInputStream(SCRIPT.getBytes(StandardCharsets.UTF_8));
         List<String> expect = Arrays.asList(SCRIPT.split("\n"));
 
-
-        when(resourceLoader.getResource(FILE_PATH)).thenReturn(resource);
-        when(resource.getInputStream()).thenReturn(inputStream);
+        inputStream();
 
         List<String> result = resourcesFile.getData(FILE_PATH);
 
         assertThat(result).isEqualTo(expect);
         verify(resourceLoader).getResource(FILE_PATH);
+    }
+
+    @Test
+    void getScript_shouldReturnScriptAsString_whenIsValidDataProvided() throws IOException {
+        inputStream();
+
+        String result = resourcesFile.getScript(FILE_PATH).trim();
+        assertEquals(SCRIPT, result);
+    }
+
+    @Test
+    void getData_shouldReturnRuntimeException_whenResourceIsNotFound() throws IOException {
+        Resource resource = mock(Resource.class);
+
+        when(resourceLoader.getResource(FILE_PATH)).thenReturn(resource);
+        when(resource.getInputStream()).thenThrow(new IOException());
+
+        assertThrows(RuntimeException.class, () -> resourcesFile.getData(FILE_PATH));
+
+        verify(resourceLoader).getResource(FILE_PATH);
+    }
+
+    void inputStream() throws IOException {
+        Resource resource = mock(Resource.class);
+        InputStream inputStream = new ByteArrayInputStream(SCRIPT.getBytes(StandardCharsets.UTF_8));
+
+        when(resourceLoader.getResource(FILE_PATH)).thenReturn(resource);
+        when(resource.getInputStream()).thenReturn(inputStream);
     }
 }
