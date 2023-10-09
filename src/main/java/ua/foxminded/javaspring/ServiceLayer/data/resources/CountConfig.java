9db098617dbaefbdc99567e02659096cede1ee22
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-09 11:22:09 +0200

    Create CountConfig.class -Create class which contains data count student and courses

---
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/CountConfig.java
@@ -0,0 +1,18 @@
+package ua.foxminded.javaspring.ServiceLayer.data.resources;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CountConfig {
+
+    private static final Integer MAX_COUNT_COURSES_OF_STUDENT = 3;
+    private static final Integer MAX_COUNT_OF_STUDENT = 200;
+
+    public Integer getMaxCountCoursesOfStudent() {
+        return MAX_COUNT_COURSES_OF_STUDENT;
+    }
+
+    public Integer getMaxCountOfStudents(){
+        return MAX_COUNT_OF_STUDENT;
+    }
+}
