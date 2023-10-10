b9e3e7e6f68c592d38277f9ae1a7d2159d619037
”Kyrylo <kirillartemov1993@gmail.com>
2023-10-10 11:16:07 +0200

    Create GroupInitializerTest.class -Create tests for GroupInitializer.class

---
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/data/tables/GroupInitializerTest.java
@@ -0,0 +1,66 @@
+package ua.foxminded.javaspring.ServiceLayer.data.tables;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import ua.foxminded.javaspring.ServiceLayer.dao.GroupDAO;
+import ua.foxminded.javaspring.ServiceLayer.data.DataConduct;
+import ua.foxminded.javaspring.ServiceLayer.model.Group;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GroupInitializerTest {
+
+	@Mock
+	private GroupDAO groupDAO;
+
+	@Mock
+	private DataConduct dataConduct;
+
+	private GroupInitializer initializer;
+
+	@BeforeEach
+	void init() {
+		MockitoAnnotations.openMocks(this);
+		initializer = new GroupInitializer(groupDAO, dataConduct);
+	}
+
+	@Test
+	void initializeGroupTablesAndData_whenGroupTableExist() {
+		Group group = new Group("group");
+		List<Group> groups = Arrays.asList(group, group, group);
+
+		when(groupDAO.isTableExist()).thenReturn(true);
+		when(groupDAO.isGroupTableEmpty()).thenReturn(true);
+		when(dataConduct.createGroups()).thenReturn(groups);
+
+		initializer.initializeGroupTablesAndData();
+
+		verify(groupDAO).isTableExist();
+		verify(groupDAO).isGroupTableEmpty();
+		verify(dataConduct).createGroups();
+	}
+
+	@Test
+	void initializeGroupTablesAndData_whenGroupTableNotExist() {
+		Group group = new Group("group");
+		List<Group> groups = Arrays.asList(group, group, group);
+
+		when(groupDAO.isTableExist()).thenReturn(false);
+		when(dataConduct.createGroups()).thenReturn(groups);
+
+		initializer.initializeGroupTablesAndData();
+
+		verify(groupDAO).isTableExist();
+		verify(dataConduct).createGroups();
+	}
+}
