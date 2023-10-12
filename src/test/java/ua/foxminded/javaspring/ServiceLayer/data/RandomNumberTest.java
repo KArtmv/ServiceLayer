650f408a8d701afcfbb5e34e3d73a53e28a4acbf
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-12 11:15:07 +0200

    Create RandomNumberTest.calss -Create test class of RandomRumber.class

---
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/data/RandomNumberTest.java
@@ -0,0 +1,53 @@
+package ua.foxminded.javaspring.ServiceLayer.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RandomNumberTest {
+
+    @Mock
+    private Random random;
+
+    private RandomNumber randomNumber;
+
+    @BeforeEach
+    void init(){
+        MockitoAnnotations.openMocks(this);
+        randomNumber = new RandomNumber(random);
+    }
+
+    @Test
+    void generateBetweenOneAnd_shouldReturnIntegerValue_whenPassedValidNumber(){
+        int passNumber = 10;
+
+        when(random.nextInt(anyInt())).thenReturn(1);
+
+        int result = randomNumber.generateBetweenOneAnd(passNumber);
+
+        assertThat((result > 0) && (result <= passNumber)).isTrue();
+
+        verify(random).nextInt(anyInt());
+    }
+
+    @Test
+    void generateBetweenOneAnd_shouldReturnIntegerValue_whenPassedNotValidNumber(){
+        int passNumber = 0;
+
+        when(random.nextInt(anyInt())).thenThrow(IllegalArgumentException.class);
+
+        assertThrows(IllegalArgumentException.class, () -> randomNumber.generateBetweenOneAnd(passNumber));
+    }
+}
