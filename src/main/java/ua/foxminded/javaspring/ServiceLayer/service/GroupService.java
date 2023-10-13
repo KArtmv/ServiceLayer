6e0e77909ee61bf7d058e1e98cc2d9103d953a9f
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-13 09:15:44 +0200

    Change GroupService -Add new method which check is Valiid Group ID

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/service/GroupService.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/service/GroupService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import ua.foxminded.javaspring.ServiceLayer.model.CounterStudentsAtGroup;
+import ua.foxminded.javaspring.ServiceLayer.model.Group;
 
 @Service
 public interface GroupService {
 
     List<CounterStudentsAtGroup> counterStudentsAtGroups(int count);
 
+    boolean isValidGroupID(Group group);
 }
--- src/main/java/ua/foxminded/javaspring/ServiceLayer/service/GroupServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/service/GroupServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import ua.foxminded.javaspring.ServiceLayer.dao.GroupDAO;
 import ua.foxminded.javaspring.ServiceLayer.model.CounterStudentsAtGroup;
+import ua.foxminded.javaspring.ServiceLayer.model.Group;
 
 public class GroupServiceImpl implements GroupService {
 
@@ -16,5 +17,10 @@ public class GroupServiceImpl implements GroupService {
     @Override
     public List<CounterStudentsAtGroup> counterStudentsAtGroups(int count) {
         return groupDAO.counterStudentsAtGroups(count);
+    }
+
+    @Override
+    public boolean isValidGroupID(Group group) {
+        return groupDAO.isValidGroupID(group);
     }
 }
