c0db1830e911d229d1887660f7fc0d5a568571a7
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-16 15:07:20 +0200

    Remove Component annotation

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/CountConfig.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/CountConfig.java
@@ -1,8 +1,5 @@
 package ua.foxminded.javaspring.ServiceLayer.data.resources;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class CountConfig {
 
     private static final Integer MAX_COUNT_COURSES_OF_STUDENT = 3;
