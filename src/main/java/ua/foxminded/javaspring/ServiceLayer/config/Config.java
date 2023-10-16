63f1f8e631a19f28f821a2e92ebf3c2cfe90d666
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-16 15:20:14 +0200

    Add new beans -Add bean of ResourcesFilesDatabaseData.class -Add bean of SQLFilesOfCreateTables.class -Add bean of SQLScriptTablesExist.class

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/config/Config.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/config/Config.java
@@ -3,12 +3,29 @@ package ua.foxminded.javaspring.ServiceLayer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.CountConfig;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.ResourcesFilesDatabaseData;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLFilesOfCreateTables;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLScriptTablesExist;
 
 @Configuration
 public class Config {
+    @Bean
+    public CountConfig countConfig() {
+        return new CountConfig();
+    }
 
     @Bean
-    public CountConfig countConfig (){
-        return new CountConfig();
+    public ResourcesFilesDatabaseData resourcesFilesDatabaseData() {
+        return new ResourcesFilesDatabaseData();
+    }
+
+    @Bean
+    public SQLFilesOfCreateTables filesOfCreateTables() {
+        return new SQLFilesOfCreateTables();
+    }
+
+    @Bean
+    public SQLScriptTablesExist scriptTablesExist() {
+        return new SQLScriptTablesExist();
     }
 }
