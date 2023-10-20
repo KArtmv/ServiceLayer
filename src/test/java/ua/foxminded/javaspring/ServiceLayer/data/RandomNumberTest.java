0f731ee7cb10ffc8a2e74fb56bdb078ef9988faf
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-20 15:32:22 +0200

    Create new test

--- src/test/java/ua/foxminded/javaspring/ServiceLayer/data/RandomNumberTest.java
+++ src/test/java/ua/foxminded/javaspring/ServiceLayer/data/RandomNumberTest.java
@@ -1,53 +1,18 @@
 package ua.foxminded.javaspring.ServiceLayer.data;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.Random;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class RandomNumberTest {
 
-    @Mock
-    private Random random;
-
-    private RandomNumber randomNumber;
-
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
-        randomNumber = new RandomNumber();
-    }
-
     @Test
-    void generateBetweenOneAnd_shouldReturnIntegerValue_whenPassedValidNumber() {
-        int passNumber = 10;
+    public void generateBetweenOneAnd_shouldReturnIntegerValue_whenIsCalled(){
+        RandomNumber randomNumber = new RandomNumber();
 
-        when(random.nextInt(anyInt())).thenReturn(1);
-
-        int result = randomNumber.generateBetweenOneAnd(passNumber);
-
-        assertThat((result > 0) && (result <= passNumber)).isTrue();
-
-        verify(random).nextInt(anyInt());
-    }
-
-    @Test
-    void generateBetweenOneAnd_shouldReturnIntegerValue_whenPassedNotValidNumber() {
-        int passNumber = 0;
-
-        when(random.nextInt(anyInt())).thenThrow(IllegalArgumentException.class);
-
-        assertThrows(IllegalArgumentException.class, () -> randomNumber.generateBetweenOneAnd(passNumber));
+        for(int inputNumber = 1; inputNumber <= 20; inputNumber++){
+            int result = randomNumber.generateBetweenOneAndInputNumber(inputNumber);
+            assertThat((result >= 1)&&(result <= inputNumber)).isTrue();
+        }
     }
 }
