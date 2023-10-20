5a1d6d269405d58db6d0e69318b976532b7c21fe
Kyrylo Artemov <kirillartemov1993@gmail.com>
2023-10-20 15:41:04 +0200

    Refactor GeneratorDatabaseData class -Remove StudentInitializer, CourseInitializer, GroupInitializer, StudentToCourseInitializer from class member, instead instanstiate that manualy -Add GroupDAO, CourseDAO, StudentDAO, StudentAtCourseDAO, DataConduct, ReadResourcesFile to constructor -Add instanstiate SQLQueryIsTableExist, SQLQueryOfCreateTable

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/GeneratorDatabaseData.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/GeneratorDatabaseData.java
@@ -1,38 +1,62 @@
 package ua.foxminded.javaspring.ServiceLayer.data;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
-
+import ua.foxminded.javaspring.ServiceLayer.dao.CourseDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.GroupDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.StudentAtCourseDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.StudentDAO;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLQueryIsTableExist;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLQueryOfCreateTable;
 import ua.foxminded.javaspring.ServiceLayer.data.tables.CourseInitializer;
 import ua.foxminded.javaspring.ServiceLayer.data.tables.GroupInitializer;
 import ua.foxminded.javaspring.ServiceLayer.data.tables.StudentInitializer;
 import ua.foxminded.javaspring.ServiceLayer.data.tables.StudentToCourseInitializer;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class GeneratorDatabaseData {
 
-    private StudentInitializer studentInitializer;
-    private CourseInitializer courseInitializer;
-    private GroupInitializer groupInitializer;
-    private StudentToCourseInitializer studentToCourseInitializer;
+    private GroupDAO groupDAO;
 
-    public GeneratorDatabaseData(StudentInitializer studentInitializer, CourseInitializer courseInitializer,
-                                 GroupInitializer groupInitializer, StudentToCourseInitializer studentToCourseInitializer) {
-        this.studentInitializer = studentInitializer;
-        this.courseInitializer = courseInitializer;
-        this.groupInitializer = groupInitializer;
-        this.studentToCourseInitializer = studentToCourseInitializer;
+    private CourseDAO courseDAO;
+
+    private StudentDAO studentDAO;
+
+    private StudentAtCourseDAO studentAtCourseDAO;
+
+    private DataConduct dataConduct;
+
+    private ReadResourcesFile readResourcesFile;
+
+    private SQLQueryIsTableExist isTableExist = new SQLQueryIsTableExist();
+
+    private SQLQueryOfCreateTable createTable = new SQLQueryOfCreateTable();
+
+    @Autowired
+    public GeneratorDatabaseData(GroupDAO groupDAO, CourseDAO courseDAO, StudentDAO studentDAO, StudentAtCourseDAO studentAtCourseDAO, DataConduct dataConduct, ReadResourcesFile readResourcesFile) {
+        this.groupDAO = groupDAO;
+        this.courseDAO = courseDAO;
+        this.studentDAO = studentDAO;
+        this.studentAtCourseDAO = studentAtCourseDAO;
+        this.dataConduct = dataConduct;
+        this.readResourcesFile = readResourcesFile;
     }
 
     @PostConstruct
     public void generateDataAndInsertIntoDatabase() {
+        GroupInitializer groupInitializer = new GroupInitializer(groupDAO, dataConduct, readResourcesFile, isTableExist, createTable);
         groupInitializer.initializeGroupTablesAndData();
 
+        CourseInitializer courseInitializer = new CourseInitializer(courseDAO, dataConduct, readResourcesFile, isTableExist, createTable);
         courseInitializer.initializeCourseTableAndData();
 
+        StudentInitializer studentInitializer = new StudentInitializer(studentDAO, dataConduct, readResourcesFile, isTableExist, createTable);
         studentInitializer.initializeStudentTableAndData();
 
+        StudentToCourseInitializer studentToCourseInitializer = new StudentToCourseInitializer(studentAtCourseDAO, dataConduct, readResourcesFile, isTableExist, createTable);
         studentToCourseInitializer.initializeStudentToCourseTableAndData();
     }
 }
