3a0ec45e2a20a2afe91735b0a6a8b8d01ab5d540
”Kyrylo <kirillartemov1993@gmail.com>
2023-10-23 14:53:23 +0200

    Remove unnecessary import

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/DataConduct.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/DataConduct.java
@@ -2,8 +2,6 @@ package ua.foxminded.javaspring.ServiceLayer.data;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 import ua.foxminded.javaspring.ServiceLayer.data.generator.CourseGenerator;
 import ua.foxminded.javaspring.ServiceLayer.data.generator.GroupGenerator;
 import ua.foxminded.javaspring.ServiceLayer.data.generator.StudentGenerator;
@@ -15,48 +13,49 @@ import ua.foxminded.javaspring.ServiceLayer.model.StudentAtCourse;
 
 public class DataConduct {
 
-    private StudentGenerator studentGenerator;
-    private CourseGenerator courseGenerator;
-    private GroupGenerator groupGenerator;
-    private StudentToCourseGenerator studentToCourse;
+	private StudentGenerator studentGenerator;
+	private CourseGenerator courseGenerator;
+	private GroupGenerator groupGenerator;
+	private StudentToCourseGenerator studentToCourse;
 
-    private List<Student> students;
-    private List<Course> courses;
-    private List<Group> groups;
+	private List<Student> students;
+	private List<Course> courses;
+	private List<Group> groups;
 
-    public DataConduct(StudentGenerator studentGenerator, CourseGenerator courseGenerator,
-                       GroupGenerator groupGenerator, StudentToCourseGenerator studentToCourse) {
-        this.studentGenerator = studentGenerator;
-        this.courseGenerator = courseGenerator;
-        this.groupGenerator = groupGenerator;
-        this.studentToCourse = studentToCourse;
-    }
+	public DataConduct(StudentGenerator studentGenerator, CourseGenerator courseGenerator,
+			GroupGenerator groupGenerator, StudentToCourseGenerator studentToCourse) {
+		this.studentGenerator = studentGenerator;
+		this.courseGenerator = courseGenerator;
+		this.groupGenerator = groupGenerator;
+		this.studentToCourse = studentToCourse;
+	}
 
-    public List<Student> createStudents() {
-        if (students == null) {
-            createGroups();
-            students = studentGenerator.generate(groups);
-        }
-        return students;
-    }
+	public List<Student> createStudents() {
+		
+		if (students == null) {
+			createGroups();
+			students = studentGenerator.generate(groups);
+		}
+		return students;
+	}
 
-    public List<Group> createGroups() {
-        if (groups == null) {
-            groups = groupGenerator.generate();
-        }
-        return groups;
-    }
+	public List<Group> createGroups() {
+		if (groups == null) {
+			groups = groupGenerator.generate();
+		}
+		return groups;
+	}
 
-    public List<Course> createCourses() {
-        if (courses == null) {
-            courses = courseGenerator.generate();
-        }
-        return courses;
-    }
+	public List<Course> createCourses() {
+		if (courses == null) {
+			courses = courseGenerator.generate();
+		}
+		return courses;
+	}
 
-    public List<StudentAtCourse> createRelationStudentCourse() {
-        createCourses();
-        createStudents();
-        return studentToCourse.addStudentToCourse(students, courses.size());
-    }
+	public List<StudentAtCourse> createRelationStudentCourse() {
+		createCourses();
+		createStudents();
+		return studentToCourse.addStudentToCourse(students, courses.size());
+	}
 }
