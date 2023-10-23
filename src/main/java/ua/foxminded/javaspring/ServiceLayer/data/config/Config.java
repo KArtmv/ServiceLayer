d07754ca497d0afb1ffc9f93fc3da80d6892e004
”Kyrylo <kirillartemov1993@gmail.com>
2023-10-23 14:39:16 +0200

    Refactor Config.class -Add autowired of GroupDAO, CourseDAO, StudentDAO  StudentAtCourseDAO.class -Add instance of ReadResourceFile as class memeber instaed to  retursn  every time new instance by called readFile() -Crate bean of GroupInitializer, CourseInitializer, StudentInitializer,  StudentToCourseInitializer -Remove RandomNumber, ResourcesFilesDatabaseData, CountConfig  from class intead that these passed as new instance

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/config/Config.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/config/Config.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import ua.foxminded.javaspring.ServiceLayer.dao.CourseDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.GroupDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.StudentAtCourseDAO;
+import ua.foxminded.javaspring.ServiceLayer.dao.StudentDAO;
 import ua.foxminded.javaspring.ServiceLayer.data.DataConduct;
 import ua.foxminded.javaspring.ServiceLayer.data.RandomNumber;
 import ua.foxminded.javaspring.ServiceLayer.data.ReadResourcesFile;
@@ -13,42 +18,81 @@ import ua.foxminded.javaspring.ServiceLayer.data.generator.StudentGenerator;
 import ua.foxminded.javaspring.ServiceLayer.data.generator.StudentToCourseGenerator;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.CountConfig;
 import ua.foxminded.javaspring.ServiceLayer.data.resources.ResourcesFilesDatabaseData;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLQueryIsTableExist;
+import ua.foxminded.javaspring.ServiceLayer.data.resources.SQLQueryOfCreateTable;
+import ua.foxminded.javaspring.ServiceLayer.data.tables.CourseInitializer;
+import ua.foxminded.javaspring.ServiceLayer.data.tables.GroupInitializer;
+import ua.foxminded.javaspring.ServiceLayer.data.tables.StudentInitializer;
+import ua.foxminded.javaspring.ServiceLayer.data.tables.StudentToCourseInitializer;
 
 @Component
 public class Config {
 
-    @Autowired
-    private ResourceLoader resourceLoader;
+	@Autowired
+	private ResourceLoader resourceLoader;
 
-    private RandomNumber randomNumber = new RandomNumber();
+	@Autowired
+	private GroupDAO groupDAO;
 
-    private ResourcesFilesDatabaseData resourcesFiles = new ResourcesFilesDatabaseData();
+	@Autowired
+	private CourseDAO courseDAO;
 
-    private CountConfig countConfig = new CountConfig();
+	@Autowired
+	private StudentDAO studentDAO;
 
-    @Bean
-    public ReadResourcesFile readFile(){
-        return new ReadResourcesFile(resourceLoader);
-    }
+	@Autowired
+	private StudentAtCourseDAO studentAtCourseDAO;
 
-    @Bean
-    public DataConduct dataConduct(){
-        return new DataConduct(studentGenerator(), courseGenerator(), groupGenerator(), studentToCourseGenerator());
-    }
+	private ReadResourcesFile readResourcesFile;
 
-    public StudentGenerator studentGenerator(){
-        return new StudentGenerator(randomNumber, readFile(), resourcesFiles, countConfig);
-    }
+	public ReadResourcesFile readFile() {
+		readResourcesFile = new ReadResourcesFile(resourceLoader);
+		return readResourcesFile;
+	}
 
-    public CourseGenerator courseGenerator(){
-        return new CourseGenerator(readFile(), resourcesFiles);
-    }
+	@Bean
+	public GroupInitializer groupInitializer() {
+		return new GroupInitializer(groupDAO, dataConduct(), readFile(), new SQLQueryIsTableExist(),
+				new SQLQueryOfCreateTable());
+	}
 
-    public GroupGenerator groupGenerator(){
-        return new GroupGenerator(readFile(), resourcesFiles);
-    }
+	@Bean
+	public CourseInitializer courseInitializer() {
+		return new CourseInitializer(courseDAO, dataConduct(), readFile(), new SQLQueryIsTableExist(),
+				new SQLQueryOfCreateTable());
+	}
 
-    public StudentToCourseGenerator studentToCourseGenerator(){
-        return new StudentToCourseGenerator(randomNumber, countConfig);
-    }
+	@Bean
+	public StudentInitializer studentInitializer() {
+		return new StudentInitializer(studentDAO, dataConduct(), readFile(), new SQLQueryIsTableExist(),
+				new SQLQueryOfCreateTable());
+	}
+
+	@Bean
+	public StudentToCourseInitializer studentToCourseInitializer() {
+		return new StudentToCourseInitializer(studentAtCourseDAO, dataConduct(), readFile(), new SQLQueryIsTableExist(),
+				new SQLQueryOfCreateTable());
+	}
+
+	public DataConduct dataConduct() {
+		return new DataConduct(studentGenerator(), courseGenerator(), groupGenerator(), studentToCourseGenerator());
+	}
+
+	public StudentGenerator studentGenerator() {
+		return new StudentGenerator(new RandomNumber(), readFile(), new ResourcesFilesDatabaseData(),
+				new CountConfig());
+	}
+
+	public CourseGenerator courseGenerator() {
+		return new CourseGenerator(readFile(), new ResourcesFilesDatabaseData());
+	}
+
+	public GroupGenerator groupGenerator() {
+		return new GroupGenerator(readFile(), new ResourcesFilesDatabaseData());
+	}
+
+	public StudentToCourseGenerator studentToCourseGenerator() {
+		return new StudentToCourseGenerator(new RandomNumber(), new CountConfig());
+	}
+
 }
