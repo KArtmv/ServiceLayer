0729224c3f8d372818b7ffd1877ea382ab72ff02
”Kyrylo <kirillartemov1993@gmail.com>
2023-10-24 19:06:32 +0200

    Add spring-manage to inject values from the property file

--- src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/CountConfig.java
+++ src/main/java/ua/foxminded/javaspring/ServiceLayer/data/resources/CountConfig.java
@@ -1,9 +1,16 @@
 package ua.foxminded.javaspring.ServiceLayer.data.resources;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CountConfig {
 
-	private static final Integer MAX_COUNT_COURSES_OF_STUDENT = 3;
-	private static final Integer MAX_COUNT_OF_STUDENT = 200;
+	@Value("${countConfig.MAX_COUNT_COURSES_OF_STUDENT}")
+	private Integer MAX_COUNT_COURSES_OF_STUDENT;
+
+	@Value("${countConfig.MAX_COUNT_OF_STUDENT}")
+	private Integer MAX_COUNT_OF_STUDENT;
 
 	public Integer getMaxCountCoursesOfStudent() {
 		return MAX_COUNT_COURSES_OF_STUDENT;
